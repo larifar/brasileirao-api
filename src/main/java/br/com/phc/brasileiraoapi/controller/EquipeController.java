934adeb4f2c0b9638ba656f017f9ea91645a0c69
Larissa Faria <lari.f4ria@gmail.com>
2024-01-18 17:46:06 -0300

    Criação endpoints GET de equipe (listar todas e por id)

--- src/main/java/br/com/phc/brasileiraoapi/controller/EquipeController.java
+++ src/main/java/br/com/phc/brasileiraoapi/controller/EquipeController.java
@@ -1,13 +1,57 @@
 package br.com.phc.brasileiraoapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.phc.brasileiraoapi.dto.EquipeResponseDto;
+import br.com.phc.brasileiraoapi.entity.Equipe;
+import br.com.phc.brasileiraoapi.exception.StandardError;
+import br.com.phc.brasileiraoapi.service.EquipeService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api("Api de equipes")
 @RestController
 @RequestMapping("/api/v1/equipes")
 public class EquipeController {
+	
+	@Autowired
+	private EquipeService equipeService;
+	
+	@ApiOperation(value = "Buscar equipe por id")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK", response = Equipe.class),
+			@ApiResponse(code = 400, message = "Bad request", response = StandardError.class),
+			@ApiResponse(code = 401, message = "Unauthorized", response = StandardError.class),
+			@ApiResponse(code = 403, message = "Forbidden", response = StandardError.class),
+			@ApiResponse(code = 404, message = "Not found", response = StandardError.class),
+			@ApiResponse(code = 500, message = "Internal Server error", response = StandardError.class)
+	})
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Equipe> buscarEquipeId(@PathVariable("id") long id){
+		return ResponseEntity.ok().body(equipeService.buscarEquipeId(id));
+	}
+	
+	@ApiOperation(value = "Listar equipes")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "OK", response = EquipeResponseDto.class),
+			@ApiResponse(code = 400, message = "Bad request", response = StandardError.class),
+			@ApiResponse(code = 401, message = "Unauthorized", response = StandardError.class),
+			@ApiResponse(code = 403, message = "Forbidden", response = StandardError.class),
+			@ApiResponse(code = 404, message = "Not found", response = StandardError.class),
+			@ApiResponse(code = 500, message = "Internal Server error", response = StandardError.class)
+	})
+	
+	@GetMapping
+	public ResponseEntity<EquipeResponseDto> listarEquipes(){
+		return ResponseEntity.ok().body(equipeService.listarEquipes());
+	}
 
 }
--- src/main/java/br/com/phc/brasileiraoapi/service/EquipeService.java
+++ src/main/java/br/com/phc/brasileiraoapi/service/EquipeService.java
@@ -1,8 +1,28 @@
 package br.com.phc.brasileiraoapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.phc.brasileiraoapi.dto.EquipeResponseDto;
+import br.com.phc.brasileiraoapi.entity.Equipe;
+import br.com.phc.brasileiraoapi.exception.NotFoundException;
+import br.com.phc.brasileiraoapi.repository.EquipeRepository;
 
 @Service
 public class EquipeService {
+
+	@Autowired
+	private EquipeRepository equipeRepository;
+	
+	public Equipe buscarEquipeId(long id) {
+		return equipeRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("Nenhuma equipe encontrada com o id informado: " + id));
+	}
+
+	public EquipeResponseDto listarEquipes() {
+		EquipeResponseDto equipes = new EquipeResponseDto();
+		equipes.setEquipes(equipeRepository.findAll());
+		return equipes;
+	}
 
 }
